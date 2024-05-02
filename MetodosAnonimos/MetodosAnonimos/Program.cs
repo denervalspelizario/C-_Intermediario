3228fa2a38b66a2d6d965eb03be1639226041eb7
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-05-02 07:17:09 -0300

    estudo c# metodos anonimos

---
+++ MetodosAnonimos/MetodosAnonimos/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Métodos Anonimos");
+
+// criando o metodo - sintaxe do método anonimo
+Imprimir imprimir = delegate (int valor)
+{
+    Console.WriteLine($"Método Anônimo : {valor}");
+};
+
+
+// chamando o metodo
+
+imprimir(100);
+imprimir(200);
+
+Console.ReadKey();
+
+
+
+
+// criando o delegate
+public delegate void Imprimir(int valor);
