a99297c3185850af63aac1f74e2b378657c69b50
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-05-06 09:52:53 -0300

    estudo c# EXERCICIO DELEGATES

---
+++ ExercicioDelegates/ExercicioDelegates/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDelegates
+{
+    public class Pessoa
+    {
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+
+        // construtor
+        public Pessoa(string? nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        // Metodo static(podemos usar sem precisar instanciar) que retorna uma list de pessoas
+        public static List<Pessoa> GetPessoas()
+        {
+            // ja estou criando a List e adicionando as pessoas
+            List<Pessoa> pessoas = new List<Pessoa>()
+            {
+                new Pessoa("João",20),
+                new Pessoa("Maria",18),
+                new Pessoa("Pedro",25),
+                new Pessoa("Carlos",15),
+                new Pessoa("Ana",17)
+            };
+            // retornando as pessoas
+            return pessoas;
+        }
+    }
+}
---
+++ ExercicioDelegates/ExercicioDelegates/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExercicioDelegates;
+using System.Threading.Channels;
+
+Console.WriteLine("## Exercicio Delegates ##");
+
+// Definindo a lista de pessoas
+// pegando a lista de pessoas do metodo statico GetPessoas
+
+var listaDePessoas = Pessoa.GetPessoas();
+
+Console.WriteLine("1 - Imprime o nome/idade de cada pessoa usando um delegate Action<Pessoa>");
+
+Action<Pessoa> imprimirNome = x => Console.WriteLine(x.Nome + "  " + x.Idade);
+
+listaDePessoas.ForEach(imprimirNome); // percorrendo a lista de pessoas
+
+Console.WriteLine("2 - Filtra pessoas maiores de 18 anos usando um delegate Predicate<Pessoa>");
+
+Predicate<Pessoa> filtrarPessoa = x => x.Idade > 18;
+
+var pessoasMaiores18anos = listaDePessoas.FindAll(filtrarPessoa);
+
+pessoasMaiores18anos.ForEach(pessoa => Console.WriteLine(pessoa.Nome + " " + pessoa.Idade));
+
+Console.WriteLine("3 - Obtem o nome/idade da pessoa mais velha utilizando um delegate Fun<Pessoa>");
+
+Func<Pessoa, int> obterIdade = p => p.Idade;
+
+int idadeMaxima = listaDePessoas.Max(obterIdade);
+
+var pessoaMaisVelha = listaDePessoas.Find(p => p.Idade == idadeMaxima);
+
+Console.WriteLine("A pessoa mais velha é: " + pessoaMaisVelha.Nome + "idade: " + pessoaMaisVelha.Idade);
+
+Console.ReadKey();
